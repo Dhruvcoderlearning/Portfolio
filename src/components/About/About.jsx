06c67ee497d39406e8aa56277ff6a987d9302b88
Mahak <your_github_email@example.com>
2026-01-20 23:34:33 +0530

    cvupdate

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1NjCWFzW_Yhw2twlKZ6clPbs-j7uf1VKs/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1Xvo-j3iziFedlx6o2iM8c8mbEzpxc3em/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
